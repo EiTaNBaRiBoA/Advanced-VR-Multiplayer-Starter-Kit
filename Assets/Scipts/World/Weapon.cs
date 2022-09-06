c399cd7fe6fc74bdf4ac6527d8da5330cf29924d
Birgerev <bibbevidio@gmail.com>
2022-09-06 22:48:05 +0200

    Tactile vibration weapon

--- Assets/Scipts/World/Weapon.cs
+++ Assets/Scipts/World/Weapon.cs
@@ -40,7 +40,16 @@ public class Weapon : NetworkBehaviour
         
         GameObject obj = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
         NetworkServer.Spawn(obj);
-        grabbable.holdingHand.Vibrate(1, 0.2f);
         firedTime = Time.time;
+        StartCoroutine(TactileVibrationTest());
+    }
+
+    IEnumerator TactileVibrationTest()
+    {
+        grabbable.holdingHand.Vibrate(.7f, 0.05f);
+        yield return new WaitForSeconds(0.1f);
+        grabbable.holdingHand.Vibrate(1, 0.1f);
+        yield return new WaitForSeconds(0.2f);
+        grabbable.holdingHand.Vibrate(.5f, 0.05f);
     }
 }
