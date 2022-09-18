8289cf98aab26c2592a700372652f4fff3e05af5
Birgerev <bibbevidio@gmail.com>
2022-09-18 22:53:32 +0200

    Weapon first hand

--- Assets/Scipts/World/Weapon.cs
+++ Assets/Scipts/World/Weapon.cs
@@ -8,8 +8,8 @@ public class Weapon : NetworkBehaviour
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
 
-    private float firedTime;
-    private Grabbable grabbable;
+    private Grabbable _grabbable;
+    private bool _waitForReset;
 
     // Update is called once per frame
     void Update()
@@ -17,39 +17,44 @@ public class Weapon : NetworkBehaviour
         if (!hasAuthority)
             return;
 
-        grabbable = GetComponent<Grabbable>();
+        _grabbable = GetComponent<Grabbable>();
 
         TryShoot();
     }
 
     private void TryShoot()
     {
-        if (grabbable.holdingHand == null)
+        VRHand primaryHand = _grabbable.GetHandForTriggerId(0);
+        if (primaryHand == null)
             return;
-        if (grabbable.holdingHand.GetInputScheme().Trigger.ReadValue<float>() < 0.8f)
+        if (primaryHand.GetInputScheme().Trigger.ReadValue<float>() < 0.8f)
+        {
+            _waitForReset = false;
+            return;
+        }
+        if (_waitForReset)
             return;
 
+        _waitForReset = true;
         Shoot();
     }
 
     [Command]
     public void Shoot()
     {
-        if (Time.time - firedTime < 0.3f)//TODO semi auto
-            return;
-        
         GameObject obj = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
         NetworkServer.Spawn(obj);
-        firedTime = Time.time;
         StartCoroutine(TactileVibrationTest());
     }
 
     IEnumerator TactileVibrationTest()
     {
-        grabbable.holdingHand.Vibrate(.7f, 0.05f);
+        VRHand primaryHand = _grabbable.GetHandForTriggerId(0);
+        
+        primaryHand.Vibrate(.7f, 0.05f);
         yield return new WaitForSeconds(0.1f);
-        grabbable.holdingHand.Vibrate(1, 0.1f);
+        primaryHand.Vibrate(1, 0.1f);
         yield return new WaitForSeconds(0.2f);
-        grabbable.holdingHand.Vibrate(.5f, 0.05f);
+        primaryHand.Vibrate(.5f, 0.05f);
     }
 }
