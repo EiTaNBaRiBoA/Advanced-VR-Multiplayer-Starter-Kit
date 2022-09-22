abd86982535c480190620628521b8d37a5163e06
Birgerev <bibbevidio@gmail.com>
2022-09-22 20:56:51 +0200

    Event Lever

--- Assets/Scipts/World/Lever.cs
+++ Assets/Scipts/World/Lever.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Lever : NetworkBehaviour
 {
@@ -11,7 +12,9 @@ public class Lever : NetworkBehaviour
     public float maxAngle;
     [Space]
     public float value;
+    public UnityEvent<bool> stateChanged;
 
+    private bool _lastLeverUp;
     private Vector3 _startRotation;
     
     private void Start()
@@ -26,5 +29,13 @@ public class Lever : NetworkBehaviour
         
         float angleZ = Mathf.DeltaAngle(stick.localRotation.eulerAngles.z, _startRotation.z);
         value = (angleZ / (maxAngle * 2)) + 0.5f;
+
+        bool leverUp = (value > 0.5f);
+        if (leverUp != _lastLeverUp)
+        {
+            stateChanged.Invoke(leverUp);
+        }
+        
+        _lastLeverUp = leverUp;
     }
 }
