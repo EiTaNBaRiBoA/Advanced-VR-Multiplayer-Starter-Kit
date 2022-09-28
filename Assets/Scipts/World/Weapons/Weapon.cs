67142f87ce48780a3f522771235b6e98cb1be927
Birgerev <bibbevidio@gmail.com>
2022-09-28 18:55:57 +0200

    Gun tactile haptic

--- Assets/Scipts/World/Weapons/Weapon.cs
+++ Assets/Scipts/World/Weapons/Weapon.cs
@@ -19,24 +19,30 @@ public class Weapon : NetworkBehaviour
         grabbable.activated.AddListener(Shoot);
     }
     
+    public void Shoot(ActivateEventArgs args)
+    {
+        if (args.interactorObject is XRBaseControllerInteractor controllerInteractor)
+            StartCoroutine(TactileVibrationTest(controllerInteractor));
+        
+        SpawnBullet();
+        
+    }
+    
     [Command]
-    public void Shoot(ActivateEventArgs arg)
+    public void SpawnBullet()
     {
         GameObject obj = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
         NetworkServer.Spawn(obj);
             
         SoundManager.instance.Play("Shoot", bulletSpawn.position);
-        //StartCoroutine(TactileVibrationTest());
     }
 
-    /*IEnumerator TactileVibrationTest()
+    IEnumerator TactileVibrationTest(XRBaseControllerInteractor controllerInteractor)
     {
-        VRHand primaryHand = _grabbable.GetHandForTriggerId(0);
-        
-        primaryHand.Vibrate(.7f, 0.05f);
+        controllerInteractor.SendHapticImpulse(.7f, 0.05f);
         yield return new WaitForSeconds(0.1f);
-        primaryHand.Vibrate(1, 0.1f);
+        controllerInteractor.SendHapticImpulse(1, 0.1f);
         yield return new WaitForSeconds(0.2f);
-        primaryHand.Vibrate(.5f, 0.05f);
-    }*/
+        controllerInteractor.SendHapticImpulse(.5f, 0.05f);
+    }
 }
